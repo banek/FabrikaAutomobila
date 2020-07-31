233458efd552e5f212586c70aed6388fa2aa9e5b
miskovic97 <vlajko.misko@gmail.com>
2020-07-31 14:38:03 +0200

    Dodavanje klase parking i razvijanje logike za vracanje slobodnog mesta na parkingu

---
+++ src/classes/Parking.java
@@ -0,0 +1,77 @@
+package classes;
+
+import java.util.ArrayList;
+
+
+
+public class Parking {
+	private String tipParkinga;
+	private int povrsinaParkinga;
+	private int visinaParkinga;
+	ArrayList<PrevoznoSredstvo> listaParkiranihPrevoznihSredstava = new ArrayList<PrevoznoSredstvo>();
+
+	public Parking() {
+	}
+
+	public Parking(String tipParkinga, int povrsinaParkinga, int visinaParkinga) {
+		
+		this.tipParkinga = tipParkinga;
+		this.povrsinaParkinga = povrsinaParkinga;
+		this.visinaParkinga = visinaParkinga;
+	}
+
+	public ArrayList<PrevoznoSredstvo> getListaParkiranihPrevoznihSredstava() {
+		return listaParkiranihPrevoznihSredstava;
+	}
+
+	public void setListaParkiranihPrevoznihSredstava(ArrayList<PrevoznoSredstvo> listaParkiranihPrevoznihSredstava) {
+		this.listaParkiranihPrevoznihSredstava = listaParkiranihPrevoznihSredstava;
+	}
+
+	public String getTipParkinga() {
+		return tipParkinga;
+	}
+
+	public void setTipParkinga(String tipParkinga) {
+		this.tipParkinga = tipParkinga;
+	}
+
+	public int getPovrsinaParkinga() {
+		return povrsinaParkinga;
+	}
+
+	public void setPovrsinaParkinga(int povrsinaParkinga) {
+		this.povrsinaParkinga = povrsinaParkinga;
+	}
+
+	public int getVisinaParkinga() {
+		return visinaParkinga;
+	}
+
+	public void setVisinaParkinga(int visinaParkinga) {
+		this.visinaParkinga = visinaParkinga;
+	}
+	public void dodavanjePrevoznogSredstva(PrevoznoSredstvo prevoznoSredstvo) {
+	
+		for(PrevoznoSredstvo ps :listaParkiranihPrevoznihSredstava) {
+			if(ps.equals(prevoznoSredstvo)) {
+				System.out.println("Prevozno sredstvo vec postoji");
+				return;
+			}
+		}
+		if(slobodnaPovrsinaParkinga()<=prevoznoSredstvo.getPovrsinaUM2()) {
+			System.out.println("Nema dovoljno mesta da bi se vozilo ubacilo");
+		}else {
+		listaParkiranihPrevoznihSredstava.add(prevoznoSredstvo);
+		}
+	}
+	private int slobodnaPovrsinaParkinga() {
+		int zazetapovrsina=0;
+		for(PrevoznoSredstvo ps: listaParkiranihPrevoznihSredstava) {
+			
+			zazetapovrsina+=ps.getPovrsinaUM2();
+		}
+		return povrsinaParkinga-zazetapovrsina;
+		
+	}
+}
