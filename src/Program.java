bb2f4835c22d0b567cb90836559f8a985df5369a
DjoleGav99 <gavrancic.djole@gmail.com>
2020-08-03 14:45:27 +0200

    U mainu radjena provera klase servis

--- src/Program.java
+++ src/Program.java
@@ -1,15 +1,33 @@
+import classes.Parking;
 import classes.PraviVoziloFactory;
+import classes.PrevoznoSredstvo;
+import classes.Servis;
 import classes.TipVozila;
 
 public class Program {
 
 	public static void main(String[] args) {
+
+		PraviVoziloFactory fabrikaVozila = PraviVoziloFactory.vratiInstancu();
+
+		/*Parking otvoreniParking = new Parking();
+		otvoreniParking.setTipParkinga(TipParkinga.OTVOREN);
+		otvoreniParking.setPovrsinaParkinga(1000);*/
+		Servis s = new Servis();
 		
-		PraviVoziloFactory ps = PraviVoziloFactory.vratiInstancu();
-		ps.napraviVozilo(TipVozila.KABRIOLET);
+		PrevoznoSredstvo prevoznosredstvo = s.PrimiPorudzbenicuIKreiraj(TipVozila.CISTERNA);
+		s.parkirajVozilo(prevoznosredstvo);
 		
-		
-		
+		PrevoznoSredstvo cist = fabrikaVozila.napraviVozilo(TipVozila.CISTERNA);
+		s.parkirajVozilo(cist);
+		PrevoznoSredstvo kab = fabrikaVozila.napraviVozilo(TipVozila.KABRIOLET);
+		s.parkirajVozilo(kab);
+		PrevoznoSredstvo autobus = fabrikaVozila.napraviVozilo(TipVozila.MEDJUGRADSKI_AUTOBUS);
+		s.parkirajVozilo(autobus);
+		for (Parking parking : fabrikaVozila.getListaParkinga()) {
+			System.out.println(parking.getListaParkiranihPrevoznihSredstava().size());
+		}
+
 	}
 
 }
