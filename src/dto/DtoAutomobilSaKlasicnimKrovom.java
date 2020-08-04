fd2733796e65b49236d9b334f350c6355460e464
miskovic97 <vlajko.misko@gmail.com>
2020-08-04 11:38:32 +0200

    Hot fix - dodavanje metoda castToEntity u klasu kabriolet i automobil sa klasicnim krovom

--- src/dto/DtoAutomobilSaKlasicnimKrovom.java
+++ src/dto/DtoAutomobilSaKlasicnimKrovom.java
@@ -1,5 +1,20 @@
 package dto;
 
+import classes.AutomobilSaKlasicnimKrovom;
+
 public class DtoAutomobilSaKlasicnimKrovom extends DtoAutomobil{
 	public double nosivostKrovaUKg;
+	
+	public AutomobilSaKlasicnimKrovom castToEntity() {
+		AutomobilSaKlasicnimKrovom aut = new AutomobilSaKlasicnimKrovom();
+		aut.setBoja(this.boja);
+		aut.setBrojSasije(this.brojSasije);
+		aut.setBrojSedista(this.brojSedista);
+		aut.setListaDodatneOpreme(this.listaDodatneOpreme);
+		aut.setNazivMarke(this.nazivMarke);
+		aut.setNosivostKrovaUKg(this.nosivostKrovaUKg);
+		aut.setTipVozila(this.tipVozila);
+		return aut;
+		
+	}
 }
--- src/dto/DtoKabriolet.java
+++ src/dto/DtoKabriolet.java
@@ -1,6 +1,21 @@
 package dto;
 
+import classes.Kabriolet;
+
 public class DtoKabriolet extends DtoAutomobil{
 	public boolean mogucnostSkidanjaKrova;
 	public boolean mogucnostPakovanjaKrovaUPrtljaznik;
+	
+	public Kabriolet castToEntity() {
+		Kabriolet kabriolet = new Kabriolet();
+		kabriolet.setBoja(this.boja);
+		kabriolet.setBrojSasije(this.brojSasije);
+		kabriolet.setBrojSedista(this.brojSedista);
+		kabriolet.setListaDodatneOpreme(this.listaDodatneOpreme);
+		kabriolet.setMogucnostPakovanjaKrovaUPrtljaznik(this.mogucnostPakovanjaKrovaUPrtljaznik);
+		kabriolet.setMogucnostSkidanjaKrova(this.mogucnostSkidanjaKrova);
+		kabriolet.setNazivMarke(this.nazivMarke);
+		kabriolet.setTipVozila(this.tipVozila);
+		return kabriolet;
+	}
 }
