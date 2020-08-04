6ba1a4c9b19b42cb617709c63269ebda1235d75e
Milos Tomasevic <milosnstomasevic@gmail.com>
2020-08-04 10:34:12 +0200

    Napravljene izmene

--- src/classes/Cisterna.java
+++ src/classes/Cisterna.java
@@ -2,17 +2,19 @@ package classes;
 
 public class Cisterna extends Kamion {
 	private double kapacitetTanka;
+	private final int POVRSINACISTERNA = 20;
 
+	
 //konstruktor
 	public Cisterna() {
 		super();
-		povrsinaUM2=20;
+		povrsinaUM2=POVRSINACISTERNA;
 	}
 
 	public Cisterna(TipVozila tipVozila, String boja, String nazivMarke, int povrsinaUM2, int brojOsovina, double kapacitetTanka) {
 		super(tipVozila, boja, nazivMarke, povrsinaUM2,brojOsovina);
 		this.kapacitetTanka = kapacitetTanka;
-		povrsinaUM2=20;
+		povrsinaUM2=POVRSINACISTERNA;
 	}
 
 //geteri i seteri
--- src/classes/MedjugradskiAutobus.java
+++ src/classes/MedjugradskiAutobus.java
@@ -2,17 +2,19 @@ package classes;
 
 public class MedjugradskiAutobus extends Autobusi {
 	private int KapacitetPrtljaznogProstora;
+	private final int POVRSINAMEDJUGRADSKI = 15;
+
 
 	public MedjugradskiAutobus() {
 		super();
-		povrsinaUM2 = 15;
+		povrsinaUM2 = POVRSINAMEDJUGRADSKI;
 	}
 
 	public MedjugradskiAutobus(TipVozila tipVozila, String boja, String nazivMarke, int povrsinaUM2,
 			int brojMestaZaSedenje, int brojMestaZaStajanje, int kapacitetPrtljaznogProstora) {
 		super(tipVozila, boja, nazivMarke, povrsinaUM2, brojMestaZaSedenje, brojMestaZaStajanje);
 		KapacitetPrtljaznogProstora = kapacitetPrtljaznogProstora;
-		povrsinaUM2 = 15;
+		povrsinaUM2 = POVRSINAMEDJUGRADSKI;
 	}
 
 	public int getKapacitetPrtljaznogProstora() {
